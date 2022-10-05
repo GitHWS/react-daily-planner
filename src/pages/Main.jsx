14e2cff6010b1e64606c9dd6d301cf32834fbae2
GitHWS <gitcodehw@gmail.com>
2022-10-05 18:17:04 +0900

    feat : UI layout complete

---
+++ src/pages/Main.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from "styled-components";
+
+import { Text } from "../components/Text";
+
+const Main = () => {
+  return (
+    <MainContainer>
+      <TitleContentBox>
+        <Text type="title" className="title">
+          Daily Planner
+        </Text>
+      </TitleContentBox>
+      <ContentHalfBox>쿼트박스</ContentHalfBox>
+      <ContentHalfBox>목표박스</ContentHalfBox>
+      <TodayPlanBox></TodayPlanBox>
+      <TimeTable>타임테이블</TimeTable>
+    </MainContainer>
+  );
+};
+
+export default Main;
+
+const MainContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: repeat(6, 1fr);
+  margin: 0 auto;
+  width: 90vw;
+  height: 100vh;
+  border: 1px solid black;
+`;
+
+const TitleContentBox = styled.div`
+  display: grid;
+  grid-template-rows: 0.3fr 1fr;
+  grid-template-columns: 1fr 1fr;
+  grid-column: 1 / 3;
+  border-bottom: 1px solid black;
+
+  & .title {
+    grid-column: 1 / 3;
+  }
+`;
+
+const ContentHalfBox = styled.div`
+  grid-template-rows: 0.3fr 1fr;
+  border-bottom: 1px solid black;
+  border-right: 1px solid black;
+
+  & + & {
+    border-right: none;
+  }
+`;
+
+const TodayPlanBox = styled.div`
+  display: grid;
+  grid-template-rows: 30px 1fr 0.5fr;
+  grid-row: 3 / 7;
+
+  border-bottom: 1px solid black;
+  border-right: 1px solid black;
+`;
+
+const TimeTable = styled.div`
+  display: grid;
+  grid-template-rows: 30px 1fr;
+  grid-row: 3 / 7;
+`;
