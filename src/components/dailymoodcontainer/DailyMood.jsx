9303528bb328ea51b87190ba326da4820bc3254a
GitHWS <gitcodehw@gmail.com>
2022-10-06 16:19:53 +0900

    feat : set dailyMood

--- src/components/dailymoodcontainer/DailyMood.jsx
+++ src/components/dailymoodcontainer/DailyMood.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import DailyMoodView from "./DailyMoodView";
 
 const DailyMood = () => {
-  return <div>DailyMood</div>;
+  const [selectedMood, setSelectedMood] = useState("");
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setSelectedMood(e.target.value);
+  };
+
+  return <DailyMoodView handleChange={handleChange} />;
 };
 
 export default DailyMood;
